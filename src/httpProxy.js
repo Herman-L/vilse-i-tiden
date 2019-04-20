bcef1f2e01a38a7f579c6c7b07509cf40c78e4d9
Herman Lauenstein <herman.lauenstein@gmail.com>
2019-04-20 17:20:36 +0200

    Kopera bara content-type headern

--- src/httpProxy.js
+++ src/httpProxy.js
@@ -3,7 +3,9 @@ const https = require('https');
 function httpProxy(destination) {
     return function(request, response, next) {
         https.get(destination + request.url, res => {
-            response.writeHead(res.statusCode, res.headers);
+            response.writeHead(res.statusCode, {
+                'content-type': res.headers['content-type'],
+            });
             res.on('data', data => response.write(data));
             res.on('end', () => response.end());
         });
