0b5e844cf17543abbe1545d5ba010152d223cd78
Herman Lauenstein <herman.lauenstein@gmail.com>
2019-04-20 16:46:43 +0200

    Implementera en enkel proxy

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+const express = require('express');
+const https = require('https');
+
+const server = express();
+const PORT = 8000;
+
+server.use('/', (req, res, next) => {
+    https.get('https://media.svt.se/spel/vintergatan' + req.path, response => {
+        res.writeHead(response.statusCode, response.headers);
+        response.on('data', data => res.write(data));
+        response.on('end', () => res.end());
+    });
+});
+
+server.listen(PORT, () => console.log(`Server startad på http://localhost:${PORT}/`));
