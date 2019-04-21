7819e8c2f566abd7b4848992769bdc8fecbcdc15
Herman Lauenstein <herman.lauenstein@gmail.com>
2019-04-21 12:05:21 +0200

    Lägg till förladdning av filer

--- src/httpProxy.js
+++ src/httpProxy.js
@@ -1,7 +1,34 @@
+const fs = require('fs').promises;
 const https = require('https');
+const path = require('path');
 
-function httpProxy(destination) {
+async function httpProxy(destination) {
     let cache = new Map();
+
+    function loadCache(path) {
+        return new Promise((resolve, reject) => {
+            https.get(destination + path, res => {
+                let status = res.statusCode;
+                let headers = {
+                    'content-type': res.headers['content-type'],
+                    'cache-control': ['no-cache', 'no-store', 'must-revalidate'],
+                    'pragma': 'no-cache',
+                    'expires': 0,
+                };
+                let fileData = [];
+
+                res.on('data', data => fileData.push(data));
+                res.on('end', () => {
+                    cache.set(path, {
+                        status: status,
+                        headers: headers,
+                        data: Buffer.concat(fileData),
+                    });
+                    resolve();
+                });
+            });
+        });
+    }
 
     function writeCached(response, path) {
         let entry = cache.get(path);
@@ -10,30 +37,17 @@ function httpProxy(destination) {
         response.end();
     }
 
-    return function(request, response, next) {
-        if (cache.has(request.path))
-            return writeCached(response, request.path);
+    console.log('Cachar filer');
+    let lines = await fs.readFile(path.join(__dirname, 'files.txt'), 'UTF-8');
+    await Promise.all(lines.split('\n')
+        .filter(s => s.length > 0)
+        .map(s => loadCache(s)));;
 
-        https.get(destination + request.url, res => {
-            let status = res.statusCode;
-            let headers = {
-                'content-type': res.headers['content-type'],
-                'cache-control': ['no-cache', 'no-store', 'must-revalidate'],
-                'pragma': 'no-cache',
-                'expires': 0,
-            };
-            let fileData = [];
+    return async function(request, response, next) {
+        if (!cache.has(request.path))
+            await loadCache(request.path);
 
-            res.on('data', data => fileData.push(data));
-            res.on('end', () => {
-                cache.set(request.path, {
-                    status: status,
-                    headers: headers,
-                    data: Buffer.concat(fileData),
-                });
-                writeCached(response, request.path);
-            });
-        });
+        writeCached(response, request.path);
     };
 }
 
