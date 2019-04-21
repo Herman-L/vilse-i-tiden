2f772b147be343cc35bb3d0e280a12e947753c1d
Herman Lauenstein <herman.lauenstein@gmail.com>
2019-04-21 15:47:11 +0200

    Implementera timern

--- src/timer.js
+++ src/timer.js
@@ -4,9 +4,44 @@ class Timer extends EventEmitter {
     constructor(config) {
         super();
         this.config = config;
+        this.reset();
+    }
+    reset() {
+        this.startTime = null;
+        this.endTime = null;
+        this.segment = -1;
+    }
+    nextTrigger() {
+        let trigger = this.segment + 1 < this.config.segments.length ?
+            this.config.segments[this.segment + 1].trigger :
+            this.config.endTrigger;
+
+        return Array.isArray(trigger) ? trigger : [trigger, 0];
     }
     process(path) {
-        console.log(path);
+        if (path === '/')
+            this.reset();
+
+        if (this.endTime !== null)
+            return;
+
+        let [triggerPath, triggerOffset] = this.nextTrigger();
+        if (triggerPath == path) {
+            let now = Date.now() + triggerOffset;
+
+            if (this.segment >= 0) {
+                console.log(`[${now - this.startTime}]: ${this.config.segments[this.segment].name}`);
+            } else {
+                console.log('Startar timer')
+            }
+
+            this.segment += 1;
+
+            if (this.segment == 0)
+                this.startTime = now;
+            else if(this.segment == this.config.segments.length)
+                this.endTime = now;
+        }
     }
     middleware() {
         return (request, response, next) => {
