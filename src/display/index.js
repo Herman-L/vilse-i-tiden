b5007634a4a9a397ad4f346aa8846734be7ed7be
Herman Lauenstein <herman.lauenstein@gmail.com>
2019-04-21 19:29:47 +0200

    Lägg till en timer-komponent i displayen

--- src/display/index.js
+++ src/display/index.js
@@ -11,10 +11,32 @@ class Display extends React.Component {
         socket.onmessage = message => this.setState(JSON.parse(message.data));
     }
     render() {
-        return React.createElement('div', null, [
-            React.createElement('p', null, 'Starttid: ' + (this.state.startTime && new Date(this.state.startTime))),
-            React.createElement('p', null, 'Sluttid: ' + (this.state.endTime && new Date(this.state.endTime))),
-        ]);
+        return React.createElement(Timer, {
+            startTime: this.state.startTime,
+            endTime: this.state.endTime,
+        });
+    }
+}
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            now: props.startTime,
+        };
+    }
+    componentDidMount() {
+        this.interval = setInterval(() => this.setState({
+            now: Date.now(),
+        }), 17);
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    render() {
+        let time = this.props.startTime ? (this.props.endTime || this.state.now) - this.props.startTime : 0;
+        console.log(this.props.startTime, this.props.endTime, this.state.now);
+        return React.createElement('p', null, (time / 1000).toFixed(3));
     }
 }
 
